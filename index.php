d192177653ec205bdbf314ddaece71659cafd204
MateuszSzczegura <169144559+MateuszSzczegura@users.noreply.github.com>
2024-09-19 09:13:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,29 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Energia Odnawialna</title>
+    <title>Zielona Energia</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<video autoplay muted loop class="background-video">
+        <source src="film1.mp4" type="video/mp4">
+        Twoja przeglądarka nie obsługuje odtwarzania wideo.
+    </video>
     <div class="background">
-        <div class="info-window">
-            <div class="slide">
-                <h2>Fotowoltaika</h2>
-                <p><?php $topic = 'fotowoltaika'; include 'fetch_info.php'; ?></p>
-                <button onclick="window.location.href='details.php?topic=fotowoltaika'">Dowiedz się więcej</button>
-            </div>
-            <div class="slide">
-                <h2>Turbiny Wiatrowe</h2>
-                <p><?php $topic = 'wiatraki'; include 'fetch_info.php'; ?></p>
-                <button onclick="window.location.href='details.php?topic=wiatraki'">Dowiedz się więcej</button>
-            </div>
-            <div class="slide">
-                <h2>Energia Geotermalna</h2>
-                <p><?php $topic = 'geotermalna'; include 'fetch_info.php'; ?></p>
-                <button onclick="window.location.href='details.php?topic=geotermalna'">Dowiedz się więcej</button>
+        <div class="content">
+            <div class="slideshow-container">
+                <div class="slide">
+                    <h2>Fotowoltaika</h2>
+                    <p>Doradztwo techniczne i energetyczne, projektowanie i montaż instalacji fotowoltaicznych. Optymalizacja i serwis.</p>
+                    <a href="details.php?category=fotowoltaika" class="button">Dowiedz się więcej</a>
+                </div>
+                <div class="slide">
+                    <h2>Turbiny Wiatrowe</h2>
+                    <p>Kompleksowe usługi od projektowania po serwis i demontaż turbin wiatrowych. Optymalizacja wydajności systemów.</p>
+                    <a href="details.php?category=wiatraki" class="button">Dowiedz się więcej</a>
+                </div>
+                <div class="slide">
+                    <h2>Energia Geotermalna</h2>
+                    <p>Doradztwo i badania, projektowanie i montaż systemów geotermalnych. Wiercenia i serwis pomp ciepła.</p>
+                    <a href="details.php?category=geotermia" class="button">Dowiedz się więcej</a>
+                </div>
             </div>
         </div>
     </div>
-    <script src="scripts.js"></script>
 </body>
 </html>
